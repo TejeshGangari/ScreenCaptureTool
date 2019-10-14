fda9144c7b5c0f5aa8cca2e06f9358e6d54a2819
F85TG00 <tejesh.gangari@ficosa.com>
2019-10-14 17:17:29 +0530

    Updated issues

--- src/basePackage/CaptureScreenTool.java
+++ src/basePackage/CaptureScreenTool.java
@@ -1,21 +1,18 @@
 package basePackage;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JLabel;
 import java.awt.Color;
-import javax.swing.border.EtchedBorder;
+import java.awt.EventQueue;
 import java.awt.Font;
-import javax.swing.SwingConstants;
 import java.awt.SystemColor;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
-import java.awt.Window.Type;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
+import javax.swing.border.EtchedBorder;
 
 public class CaptureScreenTool extends JFrame {
 
@@ -29,6 +26,7 @@ public class CaptureScreenTool extends JFrame {
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
+			@Override
 			public void run() {
 				try {
 					frame = new CaptureScreenTool();
@@ -48,7 +46,7 @@ public class CaptureScreenTool extends JFrame {
 		setAlwaysOnTop(true);
 		
 		CaptureScreen cs = new CaptureScreen();
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBackground(SystemColor.inactiveCaptionText);
